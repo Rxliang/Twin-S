e4ce3387d9a9b45269f7a850af717b76e2cc1a88
Hongchao <2285124906@qq.com>
2022-10-03 11:12:20 -0400

    update verification for reprojection error

--- eval/verify_hand_eye_calib.py
+++ eval/verify_hand_eye_calib.py
@@ -1,60 +1,84 @@
 import shutil
 import sys
-sys.path.append('.')
+import argparse
+from glob import glob
+sys.path.append('../')
 import numpy as np
 from util.dataLoader import dataLoader
 from util.Solver import solver
 import os
 from natsort import natsorted
 import cv2
-from preparation.stereo_vision_pipeline import getChessPoses, Q_intrinsic, draw, draw_full, chessboard_pose
+import pickle
+from preparation.stereo_vision_pipeline import getChessPoses, draw, draw_full, chessboard_pose
 ld = dataLoader()
 sol = solver()
 
 
-def get_init_frame(file_idx_list, c2m_path, num_idx, h2e_path):
+def initialization():
+    global file , args
+    
+    parser = argparse.ArgumentParser(description="Reproject charuco pattern to verify the X.")
+    parser.add_argument("--handeye_dir", dest="X_path", help="path of handeye transformation X", default='hand_eye_X.npy', type=str)
+    parser.add_argument('--outdir',dest="output_dir", help="Output directory.", required=True, type=str)
+    parser.add_argument('--cam_mtx_dir',dest="cam_mtx_dir", default='../params/zed_M_l.npy', help="camera matrix", type=str)
+    
+    args = parser.parse_args()
 
-    pose_idx = file_idx_list[num_idx]
-    cam_poses = ld.getRealPose(pose_idx, f'{calib_dir}/fwd_pose_camhand.csv')
-    _, cam_poses = sol.seven2trans(cam_poses)
-    camhand2cam = np.load(h2e_path)
-    cam2marker = np.load(c2m_path)
-    Op2marker = cam_poses@camhand2cam@cam2marker[:, :, num_idx]
-    return Op2marker
+    limg_path = args.output_dir +'/extract_poses/limg/'
+
+    if not os.path.exists(args.output_dir):
+            os.makedirs(args.output_dir)
+            print("Create Folder at Designated Address...")
+
+    if not os.path.exists(limg_path):
+        os.makedirs(limg_path)
+        print("No Designated Address limg found...")
+
+def get_init_frame(aligned_camhand_path, aligned_campose_path, num_idx, h2e_path):
+    B2H_seven = ld.getToolPose(num_idx, aligned_camhand_path)
+    W2E_seven = ld.getToolPose(num_idx, aligned_campose_path)
+    _, B2H = sol.seven2trans(B2H_seven)
+    _, W2E = sol.seven2trans(W2E_seven)
+    H2E = np.load(h2e_path)
+    E2W = sol.invTransformation(W2E)
+    # optical tracker to Marker
+    B2W = B2H@H2E@E2W
+    return B2W
 
 
-def propagate(file_idx_list, calib_dir, h2e_path, num_idx):
+def propagate(aligned_camhand_path, aligned_campose_path, timestamp_list, h2e_path, num_idx):
 
-    Op2marker = get_init_frame(file_idx_list, f'{calib_dir}/A_cam2marker.npy', num_idx, h2e_path)
-    camhand2cam = np.load(h2e_path)
-    marker2Op = sol.invTransformation(Op2marker)
+    B2W = get_init_frame(aligned_camhand_path, aligned_campose_path, num_idx, h2e_path)
+    H2E = np.load(h2e_path)
+    W2B = sol.invTransformation(B2W)
+    W2E_list = []
+    for (idx, timestamp) in enumerate(timestamp_list):
+        B2H_seven = ld.getToolPose(idx, aligned_camhand_path)
+        _, B2H = sol.seven2trans(B2H_seven)
+        W2E = sol.invTransformation(W2B@B2H@H2E)
+        W2E_list.append(W2E)
+    return W2E_list
 
-    cam2marker_list = []
-    for idx in file_idx_list:
-        cam_poses_temp = ld.getRealPose(idx, f'{calib_dir}/fwd_pose_camhand.csv')
-        _, cam_pose = sol.seven2trans(cam_poses_temp)
-        cam2marker = marker2Op@cam_pose@camhand2cam
-        cam2marker_list.append(cam2marker)
-    return cam2marker_list
 
-
-def backProject(file_idx_list, intrinsic_params_dir, cam2marker_list, calib_dir, org_imgpts_list, pattern, visual=True):
-    data_dir = calib_dir + '/rectified_left'
-    reproj_dir = calib_dir + '/reprojections'
-    shutil.rmtree(reproj_dir)
+def backProject(timestamp_list, intrinsic_params_dir, W2E_list, org_imgpts_dict, pattern, visual=True):
+    global args
+    img_dir = args.output_dir + '/extract_poses/limg'
+    reproj_dir = args.output_dir + '/reprojections'
+    if os.path.exists(reproj_dir):
+        shutil.rmtree(reproj_dir)
     os.makedirs(reproj_dir, exist_ok=True)
-    cam_mtx = Q_intrinsic(calib_dir)
-    cam_dist = np.load(intrinsic_params_dir + '/d_l.npy')
+    cam_mtx = np.load(intrinsic_params_dir)
     cam_dist = np.zeros([5])
     j = 0
     bp_error = []
-    for idx in file_idx_list:
-        marker2cam = sol.invTransformation(cam2marker_list[j])
-        R = marker2cam[:3, :3]
-        t = marker2cam[:3, 3]
+    for (idx,timestamp) in enumerate(timestamp_list):
+        W2E = W2E_list[j]
+        R = W2E[:3, :3]
+        t = W2E[:3, 3]
         rvecs, _ = cv2.Rodrigues(R)
         tvecs = t
-        width = 2
+        width = 12
         axis = np.float32([[0, 0, 0], [1.5 * width, 0, 0], [0, 1.5 * width, 0], [0, 0, 1.5 * width]]).reshape(-1, 3)
 
         # imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, cam_mtx, cam_dist)
@@ -64,33 +88,50 @@ def backProject(file_idx_list, intrinsic_params_dir, cam2marker_list, calib_dir,
         imgpts_org = np.int32(imgpts_org[0]).reshape(-1, 2)
 
         # back project error
-        bp_error.append(np.linalg.norm(imgpts[0] - org_imgpts_list[j+47]))
-        j += 1
-        if visual:
-            img = cv2.imread(os.path.join(data_dir, str(idx+47) + '_axis.png'))
+        try:
+            real_idx = idx
+            print('idx=', real_idx, 'J=', j)
+            print(np.linalg.norm(imgpts[0] - org_imgpts_dict[str(timestamp_list[real_idx])]))
+            bp_error.append(np.linalg.norm(imgpts[0] - org_imgpts_dict[str(timestamp_list[real_idx])]))
+            j += 1
+            if visual:
+                img = cv2.imread(os.path.join(img_dir, str(timestamp_list[real_idx]) + '.png'))
 
-            img = draw(img, imgpts_org, color='org')
-            # img = draw_full(img, imgpts, (7, 10), color='b')
-            img = draw_full(img, imgpts, (12, 9), color='p')
-            # cv2.imwrite(os.path.join(data_dir, str(idx) + '_axis_verify_1.png'), img)
-            cv2.imwrite(os.path.join(reproj_dir, str(idx) + '_reproj.png'), img)
+                img = draw(img, imgpts_org, color='org')
+                # img = draw_full(img, imgpts, (7, 10), color='b')
+                img = draw_full(img, imgpts, (8, 11), color='p')
+                # cv2.imwrite(os.path.join(data_dir, str(idx) + '_axis_verify_1.png'), img)
+                cv2.imwrite(os.path.join(reproj_dir, str(j) + '_reproj.png'), img)
+        except:
+            j += 1
+            continue
+        # if j >= 600:
+        #     break
     print('Mean backProj error:', np.mean(bp_error))
     return np.mean(bp_error)
 
 if __name__ == '__main__':
-    calib_dir = 'AXXB_Good_small_motion_419'
-    intrinsic_dir = 'intrinsic_params_421'
-    checkerboard_pattern = np.load('checkerboard_pattern.npy')
-    charuco_pattern = np.load('charuco_pattern.npy')
-    h2e_path = f'{calib_dir}/hand_eye_X_axxb.npy'  # axxb
+    initialization()
+    # sol.make_charuco_pattern('../params/zed_charuco_pattern.npy')
 
-    file_idx_list = np.load(f'{calib_dir}/file_idx_list.npy')[:500]
-    org_imgpts_list = np.load(f'{calib_dir}/imgpts_list.npy')[:600]
+    calib_dir = args.output_dir
+    intrinsic_dir = args.cam_mtx_dir
+    # checkerboard_pattern = np.load('../params/checkerboard_pattern.npy')
+    charuco_pattern = np.load('../params/zed_charuco_pattern.npy')
+    h2e_path = args.X_path  #
 
-    print(len(file_idx_list))
-    cam2marker_list = propagate(file_idx_list, calib_dir, h2e_path, 0)[:600]
-    RPE = backProject(file_idx_list, intrinsic_dir, cam2marker_list, calib_dir, org_imgpts_list, charuco_pattern,
+    aligned_camhand_path = f'{calib_dir}/aligned_tf_poses_camhand.csv'
+    aligned_campose_path = f'{calib_dir}/aligned_cam_pose.csv'
+    timestamp_list = ld.getTimestamp_list(aligned_campose_path)
+    with open(f'{calib_dir}/imgpts_dict.pickle', 'rb') as handle:
+        org_imgpts_dict = pickle.load(handle)
+    
+    W2E_list = propagate(aligned_camhand_path, aligned_campose_path, timestamp_list, h2e_path, 0)
+    RPE = backProject(timestamp_list, intrinsic_dir, W2E_list, org_imgpts_dict, charuco_pattern,
                 visual=True)
+    
+    
+    
     # RPE_list = []
     # for i in range(len(file_idx_list)):
     #     cam2marker_list = propagate(file_idx_list, calib_dir, h2e_path, i)
@@ -98,12 +139,3 @@ if __name__ == '__main__':
     #                 visual=True)
     #     RPE_list.append(RPE)
     # print('Mean RPE for all:', np.mean(RPE_list))
-
-    # charuco_pattern = np.zeros([108, 3])
-    # count = 0
-    # for i in range(12):
-    #     for j in range(9):
-    #         charuco_pattern[count] = np.array([i*3,j*3,0])
-    #         count += 1
-    # # print(charuco_pattern)
-    # charuco_pattern = np.save('charuco_pattern.npy', charuco_pattern)
